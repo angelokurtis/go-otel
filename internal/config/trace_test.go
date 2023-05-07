7a2c2d99fbd94e6a495fbc57a4b373f380bb2202
Tiago Angelo <kurtis.angelo@gmail.com>
2023-05-07 17:39:03 -0300

    Add unit tests for tracing configurations

--- internal/config/trace_test.go
+++ internal/config/trace_test.go
@@ -89,3 +89,111 @@ func TestTrace_ExporterOTLPTracesEndpoint(t *testing.T) {
 		assert.Equal(t, endpoint.String(), "https://otel.com.br:4317")
 	})
 }
+
+func TestTrace_ExporterOTLPTracesTimeout(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		timeout := traceConfig.ExporterOTLPTracesTimeout()
+		var oneSecond int64 = 1000
+		assert.Equal(t, timeout.Milliseconds(), oneSecond*10)
+	})
+	t.Run("", func(t *testing.T) {
+		// set the environment variables and ensure that the environment variable is cleaned up after the test
+		envvars := _test.SetEnvironmentVariables(map[string]string{
+			"OTEL_EXPORTER_OTLP_TIMEOUT": "1m",
+		})
+		defer envvars.Unset()
+
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		timeout := traceConfig.ExporterOTLPTracesTimeout()
+		var oneMinute int64 = 60000
+		assert.Equal(t, timeout.Milliseconds(), oneMinute*1)
+	})
+	t.Run("", func(t *testing.T) {
+		// set the environment variables and ensure that the environment variable is cleaned up after the test
+		envvars := _test.SetEnvironmentVariables(map[string]string{
+			"OTEL_EXPORTER_OTLP_TIMEOUT":        "10m",
+			"OTEL_EXPORTER_OTLP_TRACES_TIMEOUT": "1h",
+		})
+		defer envvars.Unset()
+
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		timeout := traceConfig.ExporterOTLPTracesTimeout()
+		var oneMinute int64 = 60000
+		assert.Equal(t, timeout.Milliseconds(), oneMinute*60)
+	})
+}
+
+func TestTrace_ExporterOTLPTracesProtocol(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		protocol := traceConfig.ExporterOTLPTracesProtocol()
+		assert.Equal(t, protocol, trace.ProtocolGrpc)
+	})
+	t.Run("", func(t *testing.T) {
+		// set the environment variables and ensure that the environment variable is cleaned up after the test
+		envvars := _test.SetEnvironmentVariables(map[string]string{
+			"OTEL_EXPORTER_OTLP_TRACES_PROTOCOL": "http/protobuf",
+		})
+		defer envvars.Unset()
+
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		protocol := traceConfig.ExporterOTLPTracesProtocol()
+		assert.Equal(t, protocol, trace.ProtocolHttpProtobuf)
+	})
+	t.Run("", func(t *testing.T) {
+		// set the environment variables and ensure that the environment variable is cleaned up after the test
+		envvars := _test.SetEnvironmentVariables(map[string]string{
+			"OTEL_EXPORTER_OTLP_PROTOCOL":        "http/protobuf",
+			"OTEL_EXPORTER_OTLP_TRACES_PROTOCOL": "grpc",
+		})
+		defer envvars.Unset()
+
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		protocol := traceConfig.ExporterOTLPTracesProtocol()
+		assert.Equal(t, protocol, trace.ProtocolGrpc)
+	})
+}
+
+func TestTrace_ExporterOTLPTracesCompression(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		compression := traceConfig.ExporterOTLPTracesCompression()
+		assert.Equal(t, compression, trace.CompressionGzip)
+	})
+	t.Run("", func(t *testing.T) {
+		// set the environment variables and ensure that the environment variable is cleaned up after the test
+		envvars := _test.SetEnvironmentVariables(map[string]string{
+			"OTEL_EXPORTER_OTLP_TRACES_COMPRESSION": "none",
+		})
+		defer envvars.Unset()
+
+		env, err := config.NewFromEnv()
+		require.NoError(t, err)
+
+		var traceConfig trace.Config = config.NewTrace(env)
+		compression := traceConfig.ExporterOTLPTracesCompression()
+		assert.Equal(t, compression, trace.CompressionNone)
+	})
+}
