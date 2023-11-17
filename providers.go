0cfa5a0bfe83db7cc88bca6b9b93bdda397ae6ca
Tiago Angelo <kurtis.angelo@gmail.com>
2023-11-16 21:55:36 -0300

    Refactor provider.go to move StartSpanFromContext function to span.go

--- providers.go
+++ providers.go
@@ -2,14 +2,10 @@ package otel
 
 import (
 	"context"
-	"runtime"
-	"strings"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/trace"
 
 	"github.com/angelokurtis/go-starter-otel/internal/env"
 	intltrace "github.com/angelokurtis/go-starter-otel/internal/trace"
@@ -56,40 +52,4 @@ func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 	return provider, func() {
 		cleanup()
 	}, nil
-}
-
-func StartSpanFromContext(ctx context.Context) (context.Context, trace.Span) {
-	provider := otel.GetTracerProvider()
-	pkg, fn := getCaller()
-	tracer := provider.Tracer(pkg)
-
-	return tracer.Start(ctx, fn)
-}
-
-func getCaller() (string, string) {
-	pc, _, _, _ := runtime.Caller(2)
-	f := runtime.FuncForPC(pc).Name()
-
-	return getPackage(f), getFunction(f)
-}
-
-func getPackage(input string) string {
-	i1 := strings.LastIndex(input, "/")
-	remaining := input[i1+1:]
-
-	i2 := strings.Index(remaining, ".")
-	if i2 == -1 {
-		return input
-	}
-
-	return input[:i1+1+i2]
-}
-
-func getFunction(input string) string {
-	index := strings.LastIndex(input, "/")
-	if index == -1 {
-		return input
-	}
-
-	return input[index+1:]
 }
