4678fc7d2403eafc2bc0c18100939efb42b834f7
Tiago Angelo <kurtis.angelo@gmail.com>
2023-11-16 21:53:34 -0300

    Refactor provider initialization and span creation

--- provider.go
+++ provider.go
@@ -2,22 +2,26 @@ package otel
 
 import (
 	"context"
+	"runtime"
+	"strings"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/angelokurtis/go-starter-otel/internal/env"
-	"github.com/angelokurtis/go-starter-otel/internal/trace"
+	intltrace "github.com/angelokurtis/go-starter-otel/internal/trace"
 )
 
-type Provider struct {
-	Tracer *sdktrace.TracerProvider
+type Providers struct {
+	TracerProvider *sdktrace.TracerProvider
 
 	propagation propagation.TextMapPropagator
 }
 
-func Init(ctx context.Context) (*Provider, func(), error) {
+func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 	r := resource.Environment()
 
 	o, err := env.LookupOTel()
@@ -27,29 +31,65 @@ func Init(ctx context.Context) (*Provider, func(), error) {
 
 	config := env.ToTrace(o)
 
-	s, err := trace.NewSampler(config)
+	s, err := intltrace.NewSampler(config)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	c, err := trace.NewTraceClient(config)
+	c, err := intltrace.NewTraceClient(config)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	se, err := trace.NewSpanExporters(ctx, config, c)
+	se, err := intltrace.NewSpanExporters(ctx, config, c)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	tp, cleanup := trace.NewTracerProvider(ctx, r, s, se, config)
-	p := trace.NewTextMapPropagator(config)
-	provider := &Provider{
-		Tracer:      tp,
-		propagation: p,
+	tp, cleanup := intltrace.NewTracerProvider(ctx, r, s, se, config)
+	p := intltrace.NewTextMapPropagator(config)
+	provider := &Providers{
+		TracerProvider: tp,
+		propagation:    p,
 	}
 
 	return provider, func() {
 		cleanup()
 	}, nil
+}
+
+func StartSpanFromContext(ctx context.Context) (context.Context, trace.Span) {
+	provider := otel.GetTracerProvider()
+	pkg, fn := getCaller()
+	tracer := provider.Tracer(pkg)
+
+	return tracer.Start(ctx, fn)
+}
+
+func getCaller() (string, string) {
+	pc, _, _, _ := runtime.Caller(2)
+	f := runtime.FuncForPC(pc).Name()
+
+	return getPackage(f), getFunction(f)
+}
+
+func getPackage(input string) string {
+	i1 := strings.LastIndex(input, "/")
+	remaining := input[i1+1:]
+
+	i2 := strings.Index(remaining, ".")
+	if i2 == -1 {
+		return input
+	}
+
+	return input[:i1+1+i2]
+}
+
+func getFunction(input string) string {
+	index := strings.LastIndex(input, "/")
+	if index == -1 {
+		return input
+	}
+
+	return input[index+1:]
 }
