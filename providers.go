2602233ea70663a4f772a5aac8d32e0e885320dd
Tiago Angelo <kurtis.angelo@gmail.com>
2023-11-22 09:28:26 -0300

    Adds detailed comments

--- providers.go
+++ providers.go
@@ -11,21 +11,27 @@ import (
 	intltrace "github.com/angelokurtis/go-starter-otel/internal/trace"
 )
 
+// Providers struct holds the TracerProvider for OpenTelemetry.
 type Providers struct {
 	TracerProvider *sdktrace.TracerProvider
 }
 
+// SetupProviders initializes and configures OpenTelemetry providers.
+// It returns a Providers struct containing the TracerProvider, a cleanup function, and an error if setup fails.
 func SetupProviders(ctx context.Context) (*Providers, func(), error) {
+	// Create a new OpenTelemetry resource.
 	r, err := intltrace.NewResource(ctx)
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to set up OpenTelemetry providers: %w", err)
 	}
 
+	// Lookup environment variables related to OpenTelemetry configuration.
 	variables, err := env.LookupVariables()
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to set up OpenTelemetry providers: %w", err)
 	}
 
+	// Create a sampler based on environment variables.
 	s, err := intltrace.NewSampler(intltrace.SamplerOptions{
 		Sampler:    env.ToTraceSampler(variables),
 		SamplerArg: env.ToTraceSamplerArg(variables),
@@ -34,6 +40,7 @@ func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 		return nil, nil, fmt.Errorf("failed to set up OpenTelemetry providers: %w", err)
 	}
 
+	// Create a client for exporting traces based on environment variables.
 	c, err := intltrace.NewClient(intltrace.ClientOptions{
 		Protocol:    env.ToTraceProtocol(variables),
 		Endpoint:    env.ToTraceEndpoint(variables),
@@ -43,6 +50,7 @@ func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 		return nil, nil, fmt.Errorf("failed to set up OpenTelemetry providers: %w", err)
 	}
 
+	// Create span exporters based on environment variables.
 	se, err := intltrace.NewSpanExporters(ctx, intltrace.SpanExportersOptions{
 		Exporters: env.ToTraceExporters(variables),
 		Client:    c,
@@ -51,8 +59,10 @@ func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 		return nil, nil, fmt.Errorf("failed to set up OpenTelemetry providers: %w", err)
 	}
 
+	// Create a text map propagator based on environment variables.
 	p := intltrace.NewTextMapPropagator(env.ToTracePropagators(variables))
 	log := logger.New()
+	// Create a TracerProvider with the configured options.
 	tp, cleanup := intltrace.NewTracerProvider(ctx, intltrace.TracerProviderOptions{
 		Resource:     r,
 		Sampler:      s,
@@ -65,6 +75,7 @@ func SetupProviders(ctx context.Context) (*Providers, func(), error) {
 		TracerProvider: tp,
 	}
 
+	// Return the initialized TracerProvider and cleanup function.
 	return provs, func() {
 		cleanup()
 	}, nil
