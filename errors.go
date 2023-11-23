8983e18995a9953f566e72d6257bf296a745c77d
Tiago Angelo <kurtis.angelo@gmail.com>
2023-11-23 11:54:23 -0300

    Wrap errors and record in OpenTelemetry span

--- errors.go
+++ errors.go
@@ -8,6 +8,8 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+// WrapError wraps the given error and records it as an error in the current OpenTelemetry span.
+// If the provided error is nil, it returns nil.
 func WrapError(ctx context.Context, err error) error {
 	if err == nil {
 		return nil
@@ -20,7 +22,30 @@ func WrapError(ctx context.Context, err error) error {
 		uerr = err
 	}
 
+	// Record the error and set its status in the span.
 	span.RecordError(uerr)
+	span.SetStatus(codes.Error, uerr.Error())
+
+	return err
+}
+
+// WrapErrorWithStack wraps the given error, records it as an error in the current OpenTelemetry span,
+// and includes the stack trace information in the recorded error.
+// If the provided error is nil, it returns nil.
+func WrapErrorWithStack(ctx context.Context, err error) error {
+	if err == nil {
+		return nil
+	}
+
+	span := trace.SpanFromContext(ctx)
+
+	uerr := errors.Unwrap(err)
+	if uerr == nil {
+		uerr = err
+	}
+
+	// Record the error with stack trace and set its status in the span.
+	span.RecordError(uerr, trace.WithStackTrace(true))
 	span.SetStatus(codes.Error, uerr.Error())
 
 	return err
