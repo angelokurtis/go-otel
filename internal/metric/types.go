27476c1534654ff22872745ba9bf9a1a67b926bf
Tiago Angelo <kurtis.angelo@gmail.com>
2023-11-26 14:36:27 -0300

    Add metric conversion functions for environment variables

--- internal/metric/types.go
+++ internal/metric/types.go
@@ -2,6 +2,20 @@
 
 package metric
 
+import (
+	"net/url"
+	"time"
+)
+
+// Exporters is a slice of Exporter, representing a collection of metric exporters.
+type Exporters []Exporter
+
+// Endpoint represents a URL endpoint for metric data export.
+type Endpoint url.URL
+
+// ExportInterval represents the interval between metric exports.
+type ExportInterval time.Duration
+
 // Compression defines the compression type to use on OTLP.
 // ENUM(none, gzip)
 type Compression string
@@ -11,5 +25,5 @@ type Compression string
 type Exporter string
 
 // Protocol defines the encoding of telemetry data and the protocol used to exchange metric data between the client and the server.
-// ENUM(g rpc, http/protobuf)
+// ENUM(grpc, http/protobuf)
 type Protocol string
