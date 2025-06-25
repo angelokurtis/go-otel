3428fa42560ff2b214a160911de18d8b319e8730
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-25 16:15:32 -0300

    Refactor trace functions for better readability

--- starter/internal/env/trace.go
+++ starter/internal/env/trace.go
@@ -15,19 +15,31 @@ func ToTraceExporters(otel *Variables, provider ExporterProvider) trace.Exporter
 }
 
 func ToTraceEndpoint(otel *Variables) trace.Endpoint {
-	return trace.Endpoint(ptr.ToDef(otel.Exporter.OTLP.Traces.Endpoint, otel.Exporter.OTLP.Endpoint))
+	ep := otel.Exporter.OTLP.Traces.Endpoint
+	def := otel.Exporter.OTLP.Endpoint
+
+	return trace.Endpoint(ptr.ToDef(ep, def))
 }
 
 func ToTraceTimeout(otel *Variables) trace.BatchTimeout {
-	return trace.BatchTimeout(ptr.ToDef(otel.Exporter.OTLP.Traces.Timeout, otel.Exporter.OTLP.Timeout))
+	t := otel.Exporter.OTLP.Traces.Timeout
+	def := otel.Exporter.OTLP.Timeout
+
+	return trace.BatchTimeout(ptr.ToDef(t, def))
 }
 
 func ToTraceProtocol(otel *Variables) trace.Protocol {
-	return ptr.ToDef(otel.Exporter.OTLP.Traces.Protocol, trace.Protocol(otel.Exporter.OTLP.Protocol))
+	p := otel.Exporter.OTLP.Traces.Protocol
+	def := otel.Exporter.OTLP.Protocol
+
+	return ptr.ToDef(p, trace.Protocol(def))
 }
 
 func ToTraceCompression(otel *Variables) trace.Compression {
-	return ptr.ToDef(otel.Exporter.OTLP.Traces.Compression, trace.Compression(otel.Exporter.OTLP.Compression))
+	c := otel.Exporter.OTLP.Traces.Compression
+	def := trace.Compression(otel.Exporter.OTLP.Compression)
+
+	return ptr.ToDef(c, def)
 }
 
 func ToTraceSampler(otel *Variables) trace.Sampler {
