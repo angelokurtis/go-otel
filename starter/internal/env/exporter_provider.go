5add844b27099ab4b7f1d90d64969eeb7ecba6a9
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-25 16:15:47 -0300

    Add LogExporters method to ExporterProvider interface

--- starter/internal/env/exporter_provider.go
+++ starter/internal/env/exporter_provider.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	intllogs "github.com/angelokurtis/go-otel/starter/internal/logs"
 	intlmetric "github.com/angelokurtis/go-otel/starter/internal/metric"
 	intltrace "github.com/angelokurtis/go-otel/starter/internal/trace"
 )
@@ -8,4 +9,5 @@ import (
 type ExporterProvider interface {
 	TraceExporters() (intltrace.Exporters, bool)
 	MetricExporters() (intlmetric.Exporters, bool)
+	LogExporters() (intllogs.Exporters, bool)
 }
